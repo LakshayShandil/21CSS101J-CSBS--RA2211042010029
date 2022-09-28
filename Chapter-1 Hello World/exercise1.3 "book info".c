08bf7cbf3573429dfa06c9f91de3bbda3950cde7
LakshayShandil <113585241+LakshayShandil@users.noreply.github.com>
2022-09-28 23:30:49 +0530

    Update exercise1.3 "book info".c

--- Chapter-1 Hello World/exercise1.3 "book info".c
+++ Chapter-1 Hello World/exercise1.3 "book info".c
@@ -1,6 +1,6 @@
-#include <studio.h>
-int main()
-{
+#include <stdio.h>
+
+int main() {
   printf("Title of the book:Programming with C\n");
   printf("Publisher:Tata Mcgraw Hill\n");
   printf("Authors:Venugopal and Sudeep\n");
