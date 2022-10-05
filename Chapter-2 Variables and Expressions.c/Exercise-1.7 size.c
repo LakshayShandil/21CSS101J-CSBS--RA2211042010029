b5699a2938b5d5c76c4b004163e5c4163894f9da
LakshayShandil <113585241+LakshayShandil@users.noreply.github.com>
2022-10-05 15:36:58 +0530

    Update Exercise-1.7 size.c

--- Chapter-2 Variables and Expressions.c/Exercise-1.7 size.c
+++ Chapter-2 Variables and Expressions.c/Exercise-1.7 size.c
@@ -2,7 +2,6 @@
 void main(){
   printf("Size of a short int is %d\n",sizeof(short int));
   printf("Size of an int is %d\n",sizeof(int));
-  printf("Size of a long int is %d\n",sizeof(long int) );
-         
-         }
+  printf("Size of a long int is %d\n",sizeof(long int));
+}
     
