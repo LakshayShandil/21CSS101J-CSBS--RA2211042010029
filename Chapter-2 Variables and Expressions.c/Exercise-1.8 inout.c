b178072c93b14477ead61dde9350896812eb4703
LakshayShandil <113585241+LakshayShandil@users.noreply.github.com>
2022-10-06 13:17:02 +0530

    Create Exercise-1.8 inout.c

---
+++ Chapter-2 Variables and Expressions.c/Exercise-1.8 inout.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main(){
+  short int shorti;
+  long int longi;
+  prrintf("Input short int:");
+  scanf("%hd",&shorti);
+  printf("Input long int:");
+  scanf("%ld",&longi);
+  printf("shorti=%hd and longi =%ld",shorti,longi);
+}
