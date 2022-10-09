1df39a7eb8891379b1e14bc45389c76af6f2e5e8
LakshayShandil <113585241+LakshayShandil@users.noreply.github.com>
2022-10-09 14:17:01 +0530

    Create Exercise-5.c

---
+++ printf/Exercise-5.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main(){
+char a,b,c;
+  a='!',b='@',c='#';
+  printf("This is the exclamation symbol:%c\n",a);
+    printf("This is the at the rate symbol:%c\n",b);
+    printf("This is the hashtag symbol:%c\n",c);
+}                                                                                          }  
