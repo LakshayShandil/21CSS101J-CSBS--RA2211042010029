608cbc5fc25acedf15a802dc0ed08213d2b3ed94
LakshayShandil <113585241+LakshayShandil@users.noreply.github.com>
2022-10-09 21:54:20 +0530

    Create Exercise-4 division.c

---
+++ scanf/Exercise-4 division.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main(){
+  int a,b,c;
+  printf("numerator:");
+  scanf("%d",&a);
+  printf("denominator");
+  scanf("%d",&b);
+  c=a/b;
+  printf("result of division:%d",c);
+}
