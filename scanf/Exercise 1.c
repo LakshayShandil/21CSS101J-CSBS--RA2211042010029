25aa3dc851bcfa81b307faca0af4ccaacfb4cc95
LakshayShandil <113585241+LakshayShandil@users.noreply.github.com>
2022-10-09 19:33:11 +0530

    Create Exercise 1.c

---
+++ scanf/Exercise 1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main(){
+  int a,b,c;
+  printf("give an integer:");
+    scanf("%d",&a);
+  printf("give second integer:");
+  scanf("%d",&b);
+  c=a+b;
+    printf("sum of the given integers:%d\n",c);
+  }
