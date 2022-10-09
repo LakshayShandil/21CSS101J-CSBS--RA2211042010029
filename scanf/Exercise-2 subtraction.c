88891613bf775f82463f2c65ae3f069e2c054550
LakshayShandil <113585241+LakshayShandil@users.noreply.github.com>
2022-10-09 21:28:14 +0530

    Create Exercise-2 subtraction.c

---
+++ scanf/Exercise-2 subtraction.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main(){
+  float a,b,c;
+  printf("give first number:");
+  scanf("%f",&a);
+  printf("give second number:");
+  scanf("%f",&b);
+  if (a>b) c=a-b;
+  else if (c=b-a);
+  printf("the difference between the two numbers:%f",c);
+  }
