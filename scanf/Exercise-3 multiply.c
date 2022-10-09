b66ea85a9f7df97a311f4c22621fd48c658a3304
LakshayShandil <113585241+LakshayShandil@users.noreply.github.com>
2022-10-09 21:45:16 +0530

    Create Exercise-3 multiply.c

---
+++ scanf/Exercise-3 multiply.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main(){
+  float a,b,c;
+  printf("input first number:");
+  scanf("%f",&a);
+  printf("input second number:");
+  scanf("%f",&b);
+  c=a*b;
+  printf("product:%f",c);
+}
