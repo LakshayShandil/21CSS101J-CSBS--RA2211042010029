9d5ea6ff0356fa4012f84cb29a5bcd477731ad78
LakshayShandil <113585241+LakshayShandil@users.noreply.github.com>
2022-11-21 02:43:29 +0530

    Create Exercise-37 codetantra Write a C program to understand different data types.c

---
+++ PROGRAM/Exercise-37 codetantra Write a C program to understand different data types.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+  int i;
+  i=10;
+  float f;
+  f=220.55;
+  char c='A';
+  printf("Integer Value = %d\n",i);
+  printf("Floating point Value = %1f\n",f);
+  printf("Character Value = %c\n,c);
+         }
