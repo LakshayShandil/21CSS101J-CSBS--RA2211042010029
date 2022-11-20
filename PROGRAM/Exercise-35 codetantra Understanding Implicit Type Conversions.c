4bbb1a78027ea921c2e35aa2e56aca9423f9f993
LakshayShandil <113585241+LakshayShandil@users.noreply.github.com>
2022-11-21 02:21:46 +0530

    Create Exercise-35 codetantra Understanding Implicit Type Conversions.c

---
+++ PROGRAM/Exercise-35 codetantra Understanding Implicit Type Conversions.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main(){
+  int i,intsum;
+  printf("i= ",i);
+  scanf("%d",&i);
+  char ch = 'a';
+  int ich = (int)ch;
+  float floatsum;
+  intsum = i+ch;
+  floatsum = i+ch;
+  printf("Integer result = %d, Float result =%f\n", intsum,flaotsum);
+}
