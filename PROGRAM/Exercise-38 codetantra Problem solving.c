09e922ca489244d43fffcf5b276ac8e52b0b7324
LakshayShandil <113585241+LakshayShandil@users.noreply.github.com>
2022-11-21 03:05:23 +0530

    Create Exercise-38 codetantra Problem solving.c

---
+++ PROGRAM/Exercise-38 codetantra Problem solving.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+  int a,b;
+  a=20,b=3;
+  printf("Addition Result = %d\n",a+b);
+  printf("Subtraction Result = %d\n",a-b);
+  printf("Multiplication Result = %d\n",a*b);
+  printf("Division Result = %d\n",a/b);
+  printf("Remainder = %d\n",a%b);
+} 
