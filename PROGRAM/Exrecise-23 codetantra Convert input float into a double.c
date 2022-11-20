13b45d3bb2cbb49cd03c2c867ee8be79df5e275c
LakshayShandil <113585241+LakshayShandil@users.noreply.github.com>
2022-11-20 20:10:29 +0530

    Create Exrecise-23 codetantra Convert input float into a double.c
    Problem Description:
    
    On one beautiful Sunday Selvan went to Aaron's house for exam preparation. 
    
    They have decided to study Mathematics subject because they have exams by coming Monday, Aaron is a master in Mathematics but Selvan is not so good in Mathematics so James trained with Selvan for getting a high score in the exam. 
    
    After teaching some problems to Selvan.Aaron have given some tasks to Selvan to solve .
    
    The problem is to convert input float into a double. Can you help Selvan in finding the solution ? 
    
    
    
    Constraints: 
    
    1.00 ≤ num1 ≤ 100.00 
    1.00 ≤ num2 ≤ 100.00 
    1.00 ≤ resnum1 ≤ 100.00 
    1.00 ≤ resnum2 ≤ 100.00 
    
    
    Input Format: 
    
    The first and second line of the input represents two different input value of type float.
    
    
    
    Output Format: 
    
    The first and second line of the output represents outputs of first and second line of input of type double.

---
+++ PROGRAM/Exrecise-23 codetantra Convert input float into a double.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main(){
+  float num1,num2;
+  scanf("%f %f ",&num1,&num2);
+  double resnum1,resnum2;
+  resnum1=num1;
+  resnum2=num2;
+  printf("%lf\n%lf\n",resnum1,resnum2);
+}  
