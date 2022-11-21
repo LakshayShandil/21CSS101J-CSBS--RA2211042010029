3a19b2d70579b4a6f71963ea095434c6bab554ad
LakshayShandil <113585241+LakshayShandil@users.noreply.github.com>
2022-11-21 13:44:06 +0530

    Create Exercise-55 DOB.c

---
+++ PROGRAM/Exercise-55 DOB.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+void main(){
+  int year,month,date;
+  printf("Enter DOB in yyyy,mm,dd format: ");
+  scanf("%d.%d.%d",&year,&month,&date);
+  printf("DOB is: %d.%d.%d\n",year,month,date);
+}
