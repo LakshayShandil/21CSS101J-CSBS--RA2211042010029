a8a67064c3fe88036278cecfccaa065496c7a568
LakshayShandil <113585241+LakshayShandil@users.noreply.github.com>
2022-11-25 00:19:54 +0530

    Create Exercise-89 set2-q2.c

---
+++ PROGRAM/Exercise-89 set2-q2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int X,Y;
+    scanf("%d %d", &X, &Y);
+
+    if(Y<=X)
+    {
+        printf("%d\n", Y);
+    }
+    else
+    {
+        printf("%d\n", X);
+    }
+}
