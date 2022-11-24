41fa69ec9124fd75083dbf23ad13f521eb8bfc39
LakshayShandil <113585241+LakshayShandil@users.noreply.github.com>
2022-11-24 21:21:39 +0530

    Create Exercise-73 swapping 2 numbers using addition and subtraction.c

---
+++ PROGRAM/Exercise-73 swapping 2 numbers using addition and subtraction.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  printf("Registration number = RA2211042010033"\n);
+  int x = 10 , y = 15;
+  x = x + y - (y = x);
+  printf("x = %d and y = %d" , x,y);
+  gtech();
+}
