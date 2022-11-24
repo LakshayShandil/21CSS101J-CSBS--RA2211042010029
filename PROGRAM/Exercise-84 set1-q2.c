e16782a24a2545ca86fbefe2352aa8d8d5743eb3
LakshayShandil <113585241+LakshayShandil@users.noreply.github.com>
2022-11-24 22:56:42 +0530

    Create Exercise-84 set1-q2.c

---
+++ PROGRAM/Exercise-84 set1-q2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+intmain()
+{
+  int a=0;
+  printf("Enter age: ");
+  scanf("%d"&a);
+  if(A>=18)
+  {
+    printf("Eligible");
+  }
+  else
+  {
+    printf("Not eleigible");
+  }
+}
