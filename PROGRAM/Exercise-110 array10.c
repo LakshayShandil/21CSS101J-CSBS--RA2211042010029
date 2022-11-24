06aae0aae7f1acce79e9ba9a14e38cb657f6ec66
LakshayShandil <113585241+LakshayShandil@users.noreply.github.com>
2022-11-25 03:28:43 +0530

    Create Exercise-110 array10.c

---
+++ PROGRAM/Exercise-110 array10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main()
+{
+    int n=0,s=0;
+    printf("Enter number of elements : ");
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+   for(int i=0;i<n;i++)
+   {
+       if(arr[i]%2==0)
+       {
+           s++;
+       }
+     printf("Number of even numbers = %d\n",s);
+   }
